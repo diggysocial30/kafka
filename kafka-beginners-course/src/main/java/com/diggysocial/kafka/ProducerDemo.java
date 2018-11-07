be99507f3dea6e168b93e06e22cb5ea5c088584a
divya <dgupta26@sapient.com>
2018-11-07 09:38:26 +0530

    Updated pom.xml

---
+++ kafka-beginners-course/src/main/java/com/diggysocial/kafka/ProducerDemo.java
@@ -0,0 +1,16 @@
+package com.diggysocial.kafka;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+
+	public static void main(String arg[]) {
+		
+	/*create producer properties
+	create producer
+	send data
+		*/
+		
+	Properties props=new Properties();	
+	}
+}
